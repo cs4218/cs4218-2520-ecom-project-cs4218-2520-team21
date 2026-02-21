8eb4d44e547b82e52abb5b93dbb2912ae6ecea20
Xenos F. <git@xenosfio.com>
2026-02-22 04:43:25 +0800

    Add useCategory hook tests

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -31,6 +31,7 @@ export default {
     "<rootDir>/client/src/pages/About.test.js",
     "<rootDir>/client/src/pages/Pagenotfound.test.js",
     "<rootDir>/client/src/pages/CartPage.test.js",
+    "<rootDir>/client/src/hooks/useCategory.test.js",
   ],
 
   // jest code coverage
@@ -43,6 +44,7 @@ export default {
     "client/src/pages/About.js",
     "client/src/pages/Pagenotfound.js",
     "client/src/pages/CartPage.js",
+    "<rootDir>/client/src/hooks/useCategory.js",
   ],
   coverageThreshold: {
     global: {
