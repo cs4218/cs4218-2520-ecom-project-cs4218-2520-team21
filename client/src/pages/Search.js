96027d10e1b251a0e7ec2cd94e91c16b6330381d
dhruvirathod29 <122886308+dhruvir29@users.noreply.github.com>
2026-03-21 22:41:11 +0800

    add end to ends tests
    fix search, add initial e2e tests

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,8 +1,15 @@
 import React from "react";
 import Layout from "./../components/Layout";
 import { useSearch } from "../context/search";
+import toast from "react-hot-toast";
+import { useCart } from "../context/cart";
+import { useParams, useNavigate } from "react-router-dom";
+
 const Search = () => {
   const [values, setValues] = useSearch();
+  const [cart, setCart] = useCart();
+  const navigate = useNavigate();
+
   return (
     <Layout title={"Search results"}>
       <div className="container">
@@ -24,11 +31,29 @@ const Search = () => {
                 <div className="card-body">
                   <h5 className="card-title">{p.name}</h5>
                   <p className="card-text">
-                    {p.description.substring(0, 30)}...
+                    {p?.description.substring(0, 30)}...
                   </p>
                   <p className="card-text"> $ {p.price}</p>
-                  <button class="btn btn-primary ms-1">More Details</button>
-                  <button class="btn btn-secondary ms-1">ADD TO CART</button>
+                  <button
+                    className="btn btn-info ms-1"
+                    onClick={() => navigate(`/product/${p.slug}`)}
+                  >
+                    More Details
+                  </button>
+                  
+                  <button
+                    className="btn btn-dark ms-1"
+                    onClick={() => {
+                      setCart([...cart, p]);
+                      localStorage.setItem(
+                        "cart",
+                        JSON.stringify([...cart, p])
+                      );
+                      toast.success("Item Added to cart");
+                    }}
+                  >
+                  ADD TO CART
+                </button>
                 </div>
               </div>
             ))}
