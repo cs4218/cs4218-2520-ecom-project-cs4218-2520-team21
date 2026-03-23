05a37202fdadc4d9133f443a83b912118e27d20f
MinkhantEain <khantkyaw.pkk@gmail.com>
2026-03-23 10:01:31 +0800

    fix server.js

--- server.js
+++ server.js
@@ -4,29 +4,13 @@ import startExpressApp from "./expressApp.js";
 
 // configure env
 dotenv.config();
-
-const app = express();
-
-//middlewares
-app.use(cors());
-app.use(express.json());
-app.use(morgan('dev'));
-
-//routes
-app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/category", categoryRoutes);
-app.use("/api/v1/product", productRoutes);
-
-// rest api
-
-app.get('/', (req,res) => {
-    res.send("<h1>Welcome to ecommerce app</h1>");
-});
+const app = startExpressApp(() => {
+    connectDB();
+})
 
 const PORT = process.env.PORT || 6060;
 
 if (process.env.NODE_ENV !== 'test') {
-    connectDB();
     app.listen(PORT, () => {
         console.log(`Server running on ${process.env.DEV_MODE} mode on ${PORT}`.bgCyan.white);
     });
